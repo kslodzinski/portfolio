48704fe843ffee6c7dd9014c306c2a68d4501592
kslodzinski <kslodzinski@protonmail.com>
2020-10-18 01:09:51 +0200

    logika dzialaasdas

--- examples/JS/toDoList with LS/script.js
+++ examples/JS/toDoList with LS/script.js
@@ -69,7 +69,7 @@ function getFromLocalStorage() {
 }
 
 // switch between checked / not checked value
-function toogle(id) {
+function toggle(id) {
     todos.forEach(function(item) {
         if ( item.id == id ) {
             item.completed = !item.completed;
@@ -93,11 +93,11 @@ getFromLocalStorage();
 
 todoItemsList.addEventListener('click', function(event) {
     if ( event.target.type === 'checkbox' ) {
-        toggle(event.target.parentElement.getAttribue('data-key'));
+        toggle(event.target.parentElement.getAttribute('data-key'));
     }
 
     if ( event.target.classList.contains('delete-button')) {
-        deleteTodo( event.target.parentElement.getAttribue('data-key'));
+        deleteTodo( event.target.parentElement.getAttribute('data-key'));
     }
 
 });
